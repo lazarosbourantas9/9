b3c8aea558ffb198254ea440ccd5f6bab79ad984
lazarosbourantas9 <lazarosbourantas9@gmail.com>
2024-11-23 19:49:07 +0200

    Add files via upload

---
+++ scripts/digitalimage.js
@@ -0,0 +1,42 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const observer2 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show2");
+        }else{
+            entry.target.classList.remove("show2");
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el)=> observer2.observe(el));
+
+
+function copyToClipboard(event, email) {
+    // Prevent the default behavior of opening the YouTube link
+    event.preventDefault();
+
+    // Create a temporary input to hold the email text
+    const tempInput = document.createElement('input');
+    tempInput.value = email; // Set the email to the input's value
+    document.body.appendChild(tempInput); // Add input to the DOM
+    tempInput.select(); // Select the content
+    document.execCommand('copy'); // Copy the content to clipboard
+    document.body.removeChild(tempInput); // Remove the input from the DOM
+
+    // Optionally show a message
+    alert('Email copied to clipboard: ' + email);
+}
