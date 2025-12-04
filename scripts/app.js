e69dbfd84e2663cb05ce22f8254a91d233fbebb2
lazarosbourantas9 <lazarosbourantas9@gmail.com>
2025-12-04 13:49:46 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-function e(e,n){const o=new IntersectionObserver(e=>{e.forEach(e=>{e.target.classList.toggle(n,e.isIntersecting)})});document.querySelectorAll(e).forEach(e=>o.observe(e))}e(".hidden","show"),e(".hidden2","show2")
+function registerObserver(className, visibleClass) {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle(visibleClass, entry.isIntersecting);
+    });
+  });
+  document.querySelectorAll(className).forEach(el => observer.observe(el));
+}
+
+registerObserver(".hidden", "show");
+registerObserver(".hidden2", "show2");
+
+
+// Copy email to clipboard
+function copyToClipboard(event, email) {
+    event.preventDefault();
+
+    const tempInput = document.createElement('input');
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    alert('Email copied to clipboard: ' + email);
+}
+
